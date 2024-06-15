ecc96f8f782fa6c7f8c1340056cc454019a79e9b
Leshi Taiwo <98643006+demi05@users.noreply.github.com>
2024-06-15 21:04:08 +0100

    started home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,21 @@
+import { Box, Container, Flex, Heading, Text } from "@chakra-ui/react";
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
+import backgroundImage from "../assets/images/images (4).jpg"
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      ghjkl
-    </div>
+    <Box backgroundImage={`url(${backgroundImage})`} backgroundSize={"cover"} backgroundPosition={"center"} backgroundRepeat={"no-repeat"}>
+      <Container>
+        <Heading>Leshi Taiwo Oluwademilade</Heading>
+        <Text>Frontend Developer</Text>
+        <Flex>
+        <Link to={"https://github.com/demi05"}><FaGithub /></Link>
+        <Link to={"https://x.com/toniiaa___"}><FaTwitter /></Link>
+        <Link to={""}><FaLinkedin /></Link>
+        </Flex>
+      </Container>
+    </Box>
   )
 }
 
