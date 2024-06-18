a78f9cbea1c3920e3f9414902a1610db966a6e8b
Leshi Taiwo <98643006+demi05@users.noreply.github.com>
2024-06-18 12:18:36 +0100

    removed unused variables

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,32 +1,13 @@
 import { Box, Container, Flex, Heading, Text } from "@chakra-ui/react";
 import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
-import myBackgroundImage from "../assets/images/images (4).jpg";
 import { Link } from "@chakra-ui/react";
 
 const Home = () => {
-  const i =0 
-  const textEl: HTMLParagraphElement | null = document.querySelector("#text-el")
-  const speed = 50
-
-//  const typeWriter = () => {
-//   if(textEl){
-//     if (i < textEl.length) {
-//       textEl.innerHTML += textEl.charAt(i);
-//       i++;
-//       setTimeout(typeWriter, speed);
-//     }
-//   }
-//   }
 
   return (
     <Container as={"section"} id="home" maxWidth={"6xl"}>
       <Flex alignItems={"center"} minH={"90vh"}>
-        <Box
-          /* backgroundImage={`url(${myBackgroundImage})`}
-          backgroundSize={"cover"}
-          backgroundPosition={"center"}
-          backgroundRepeat={"no-repeat"} */
-        >
+        <Box>
           <Flex flexDirection={"column"} gap={"1em"}>
             <Heading fontSize={"3rem"}>Leshi Taiwo Oluwademilade</Heading>
             <Text id="text-el" fontSize={"1.5rem"}>Frontend Developer</Text>
